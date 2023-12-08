b27b9333899f2302c55f0e20d19f1efc8945b5b7
gaurav-smartaxiom <gaurav@smartaxiom.com>
2023-12-08 12:08:58 +0530

    update map width with media query

--- lib/Dashboard/Dashboard.dart
+++ lib/Dashboard/Dashboard.dart
@@ -11,70 +11,78 @@ import 'package:mobileapp/Dashboard/DashboardBox.dart';
 // import 'package:bbbb/Dashboard/NotificationPage.dart';
 // import 'package:bbbb/Dashboard/userprofile.dart';
 // import 'package:bbbb/Dashboard/DashboardBox.dart';
- import 'package:url_launcher/url_launcher.dart';
- import 'package:shared_preferences/shared_preferences.dart';
- import 'package:http/http.dart' as http;
- import 'dart:convert'; 
+import 'package:url_launcher/url_launcher.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'UserManagementPage.dart';
 import 'main.dart';
 import 'package:pointer_interceptor/pointer_interceptor.dart';
 
 class DashboardPage extends StatefulWidget {
- final Map<String, dynamic> decodedToken;
+  final Map<String, dynamic> decodedToken;
   DashboardPage({required this.decodedToken});
   @override
   _DashboardPageState createState() => _DashboardPageState();
 }
+
 class _DashboardPageState extends State<DashboardPage> {
   int _selectedIndex = 0;
-  String  deviceUuid  = '' ;
- TextEditingController searchController = TextEditingController(); 
- String searchResult = ""; 
+  String deviceUuid = '';
+  TextEditingController searchController = TextEditingController();
+  String searchResult = "";
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
 
-if (index == 0) {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => DashboardPage(decodedToken: widget.decodedToken,)));
+      if (index == 0) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => DashboardPage(
+                  decodedToken: widget.decodedToken,
+                )));
       } else if (index == 1) {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ShipmentPage(decodedToken: widget.decodedToken,)));
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => ShipmentPage(
+                  decodedToken: widget.decodedToken,
+                )));
       } else if (index == 4) {
         // Navigate to UserProfilePage
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) => UserProfilePage()));
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => UserProfilePage()));
       } else if (index == 3) {
         // Navigate to NotificationPage
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) => NotificationPage(decodedToken: widget.decodedToken,)));
-      }
-      else if (index == 5) {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => NotificationPage(
+                  decodedToken: widget.decodedToken,
+                )));
+      } else if (index == 5) {
         // Navigate to NotificationPage
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) => UserManagementPage()));
-      }
-      else if(index==2)
-      
-      {
-Navigator.of(context).push(MaterialPageRoute(
-  builder: (context) => AddDevicePage(deviceUuid: '9876543210',decodedToken: widget.decodedToken,),
-));
- // Pass the device UUID
-
-
-
+        Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => UserManagementPage()));
+      } else if (index == 2) {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => AddDevicePage(
+            deviceUuid: '9876543210',
+            decodedToken: widget.decodedToken,
+          ),
+        ));
+        // Pass the device UUID
       }
     });
   }
- 
-void initState() {
+
+  void initState() {
     super.initState();
-     loadSessionData().then((token) {
+    loadSessionData().then((token) {
       // Fetch shipments from the API when the widget is initialized
-     
     });
   }
- Future<String?> loadSessionData() async {
+
+  Future<String?> loadSessionData() async {
     final sharedPreferences = await SharedPreferences.getInstance();
     final email = sharedPreferences.getString('email');
     final password = sharedPreferences.getString('password');
-    final token=sharedPreferences.getString('token');
+    final token = sharedPreferences.getString('token');
 
     print('Stored Email: $email');
     print('Stored Password: $password');
@@ -82,15 +90,8 @@ void initState() {
     return token;
   }
 
-
-
-
-
-
-
 // void _onSearchSubmitted(String query) async {
 //   final googleMapsUrl = 'https://www.google.com/maps?q=${Uri.encodeQueryComponent(query)}';
-
 
 //   if (await canLaunch(googleMapsUrl)) {
 //     print("if");
@@ -122,23 +123,23 @@ void initState() {
   @override
   Widget build(BuildContext context) {
 //print("Decoded Token and welcome  to dashboard page----------->: ${widget.decodedToken}");
-return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          centerTitle: true,
-          title: Text(
-            "Dashboard",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text(
+          "Dashboard",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
         ),
-   body: SingleChildScrollView(
+      ),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
+            /* Padding(
               padding: EdgeInsets.only(top: 10.0),
               child: Container(
                 width: 500.0,
@@ -170,13 +171,13 @@ return Scaffold(
                   ],
                 ),
               ),
-            ),
+            ), */
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  width: 410,
-                  height: 620,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height - 130,
                   child: Column(
                     children: [
                       Expanded(
@@ -200,41 +201,40 @@ return Scaffold(
           ],
         ),
       ),
-
-   bottomNavigationBar: BottomNavigationBar(
-  type: BottomNavigationBarType.fixed, // Fixed type bottom navigation bar
-  showSelectedLabels: false, // Selected label ko hide karein
-  showUnselectedLabels: false, // Unselected labels ko hide karein
-  items: const <BottomNavigationBarItem>[
-    BottomNavigationBarItem(
-      icon: Icon(Icons.window),
-      label: 'Window',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.local_shipping),
-      label: 'Shipment',
-    ),
-     BottomNavigationBarItem(
-      icon: Icon(Icons.devices_other),
-      label: 'Add_Devices',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.notifications),
-      label: 'Notifications',
-    ),
-    BottomNavigationBarItem(
-      icon: Icon(Icons.person),
-      label: 'User Profile',
-    ),
-     BottomNavigationBarItem(
-      icon: Icon(Icons.mail),
-      label: 'UserMangment',
-    ),
-  ],
-  currentIndex: _selectedIndex,
-  selectedItemColor: Colors.blue,
-  onTap: _onItemTapped,
-),
-    );   
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // Fixed type bottom navigation bar
+        showSelectedLabels: false, // Selected label ko hide karein
+        showUnselectedLabels: false, // Unselected labels ko hide karein
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.window),
+            label: 'Window',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_shipping),
+            label: 'Shipment',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.devices_other),
+            label: 'Add_Devices',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notifications',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'User Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: 'UserMangment',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
